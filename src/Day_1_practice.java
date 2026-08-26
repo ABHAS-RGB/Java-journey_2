8e3a08b43b7d0941497631120f52092b2322eb07
ABHAS-RGB <abhas4361@gmail.com>
2026-08-26 21:20:22 +0530

    practicing

---
+++ src/Day_1_practice.java
@@ -0,0 +1,77 @@
+public class Day_1_practice {
+    public static void main(String[] args) {
+//        float[] marks = {12.5f,16.7f,39.3f,55.5f};
+//        float sum =0;
+//        for (float elments:marks){
+//            sum = sum+elments;
+//        }
+// System.out.println("the value of " + sum);
+
+
+        //problem no. 2
+//        float[] marks = {12.5f,16.7f,39.3f,55.5f};
+//        float sum = 16.8f;
+//        boolean IsInArray = false;
+//        for (float elements:marks){
+//            if(sum==elements){
+//                IsInArray =true;
+//                break;
+//            }
+//        }
+//        if (IsInArray){
+//            System.out.println("present in array");
+//        }
+//        else{
+//            System.out.println("not present in array");
+//        }
+        //problem 3
+//        int sum = 0;
+//        int avg =0;
+//        int [] marks_physics = {12,7,18,0,2,9,5,15,19,20,20,2,10,13,1,20,14,1,5,6,9,8,1,3};
+//        for (int elements:marks_physics){
+//            sum = sum+elements;
+//        }
+//        System.out.println("the averge marks will "+sum/marks_physics.length);
+        //problem 4
+//        int [][]mat1 = {{1,2,3}, { 4,5,6}};
+//        int [][]mat2 = {{2,2,8}, { 8,9,3}};
+//        int [][]result = {{0,0,0}, { 0,0,0}};
+//        for (int i = 0; i< mat1.length;i++){
+//            for (int j =0;j< mat1[i].length;j++){
+//                result[i][j] =mat1[i][j] +mat2 [i][j];
+//                System.out.println(result[i][j]+" ");
+//            }
+//            System.out.println("");
+//        }
+//problem 5
+//        float[] marks = {12.5f,16.7f,39.3f,55.5f};
+//        for (int i = marks.length -1;i>=0;i--){
+//            System.out.println(marks[i]);
+//        }
+//        int[]marks = {12,16,39,55,63};
+//        int n = Math.floorDiv(marks.length,2);
+//        int temp =0;
+//        for (int i =0;i<n;i++){
+//            //swapping logic |3| |4| |3|
+//            temp = marks[i];
+//            marks[i] = marks[marks.length-i-1];
+//            marks[marks.length-i-1] = temp;
+//        }
+//        for (int elements:marks){
+//            System.out.println(elements);
+//        }
+        //problem 7
+//        int[]marks = {12,16,39,55,63};
+//        int biggest = 0;
+//        for (int e:marks){
+//            if (biggest < e){
+//                biggest = e;
+//            }
+//        }
+//        System.out.println("maximum value is" + biggest);
+    }
+
+
+
+    }
+
