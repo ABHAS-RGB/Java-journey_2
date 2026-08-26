61d0a369bd50ef7e80afb0b3bd7440f95800387f
ABHAS-RGB <abhas4361@gmail.com>
2026-08-26 13:12:03 +0530

    just changing the name of the file

---
+++ src/Day_1_Array.java
@@ -0,0 +1,5 @@
+public class Day_1_Array {
+    public static void main(String[] args) {
+        System.out.println("we are now starting array" );
+    }
+}
--- src/Day_1_methods.java
+++
@@ -1,5 +0,0 @@
-public class Day_1_methods {
-    public static void main(String[] args) {
-        System.out.println("we are now starting methods");
-    }
-}
