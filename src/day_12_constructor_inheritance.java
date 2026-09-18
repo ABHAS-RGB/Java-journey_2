fadc652601fc46dc3cc4d22c59f261a120de0a29
ABHAS-RGB <abhas4361@gmail.com>
2026-09-19 00:45:39 +0530

    keyword

--- src/day_12_constructor_inheritance.java
+++ src/day_12_constructor_inheritance.java
@@ -1,40 +1,40 @@
-class base {
-    public int x;
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    base() {
-        System.out.println("i am base class constructor");
-    }
-
-    base(int a) {
-        System.out.println(" i am an constructor with a value of a :" + a);
-
-    }
-}
-class derived extends base{
-        derived(){
-            super(0);
-            System.out.println("i am constructor");
-
-    }
-    derived(int x , int y ){
-        super(x);
-        System.out.println("i am a derived class constructor");
-    }
-
-}
-public class day_12_constructor_inheritance {
-    public static void main(String[] args) {
-        base b1 = new base();
-        derived d = new derived();
-        derived d2 = new derived();
-    }
-}
-
+//class base {
+//    public int x;
+//
+//    public int getX() {
+//        return x;
+//    }
+//
+//    public void setX(int x) {
+//        this.x = x;
+//    }
+//
+//    base() {
+//        System.out.println("i am base class constructor");
+//    }
+//
+//    base(int a) {
+//        System.out.println(" i am an constructor with a value of a :" + a);
+//
+//    }
+//}
+//class derived extends base{
+//        derived(){
+//            super(0);
+//            System.out.println("i am constructor");
+//
+//    }
+//    derived(int x , int y ){
+//        super(x);
+//        System.out.println("i am a derived class constructor");
+//    }
+//
+//}
+//public class day_12_constructor_inheritance {
+//    public static void main(String[] args) {
+//        base b1 = new base();
+//        derived d = new derived();
+//        derived d2 = new derived();
+//    }
+//}
+//
